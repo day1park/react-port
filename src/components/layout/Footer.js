239187614e5d89c72503336fc67b18bf40bfa40f
day1park <daypark2408@gmail.com>
2019-02-11 16:54:20 +1100

    yarn add react-strap

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div>
-      <h1>Footer Component</h1>
-    </div>
+    <footer className="bg-info text-light mt-5 p-4 text-center">
+      Copyright &copy; {new Date().getFullYear()} DAY PARK
+    </footer>
   );
 }
