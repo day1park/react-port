92561d4ed15fa7e6e2997f0b87f8ebd85a571f83
day1park <daypark2408@gmail.com>
2019-02-11 16:07:39 +1100

    test a footer component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import MainNavbar from "./components/layout/MainNavbar";
+import Footer from "./components/layout/Footer";
 
 class App extends Component {
   render() {
     return (
       <div>
         <MainNavbar />
-        <h1>hello universe</h1>
+        <Footer />
       </div>
     );
   }
