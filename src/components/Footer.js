be9ab8167ffb52c5bb1a9097d0f6a3cdc2aa00dc
day1park <daypark2408@gmail.com>
2019-02-15 19:26:04 +1100

    add white social icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,27 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer className="mt-5 p-4 text-center">
+      <div className="icon">
+        <a href="https://github.com/day1park" target="_blank">
+          <img src="icons/github-w.svg" />
+        </a>
+
+        <span>Github</span>
+      </div>
+      <div class="icon">
+        <a href="https://www.linkedin.com/in/day-park" target="_blank">
+          <img src="icons/linkedin-w.svg" />
+        </a>
+
+        <span>LinkedIn</span>
+      </div>
       DAY PARK &copy; {new Date().getFullYear()}
+      <span>daypark2408@gmail.com</span>
+      <span>
+        <a className="mobile" href="tel:0430126695">
+          0430 126 695
+        </a>
+      </span>
     </footer>
   );
 }
