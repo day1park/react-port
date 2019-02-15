25cec660937deffa6fc7be587f733d35618116df
day1park <daypark2408@gmail.com>
2019-02-15 17:51:30 +1100

    remove

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     return (
       <div>
         <Navbar />
-        <Main />
+
         <Footer />
       </div>
     );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,3 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Landing from "./Landing";
-
-const Main = () => (
-  <Switch>
-    <Route exact path="/" component={Landing} />
-  </Switch>
-);
-
-export default Main;
