0d8c1d23e9b42a6d64e51094d16fcb38e65af383
day1park <daypark2408@gmail.com>
2019-03-12 16:06:55 +1100

    change new component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,21 +1,30 @@
 import React, { Component } from "react";
 
+import "./Navigation.css";
+
 class Navigation extends Component {
   render() {
     return (
       <div>
         <nav>
-          <span>Day Park</span>
+          <span className="logo">
+            <a href="#">Day Park</a>
+          </span>
           <ul>
-            <li>Project</li>
-            <li>Skills</li>
-            <li>About Me</li>
-            <li>Contact</li>
+            <li>
+              <a href="#">Project</a>
+            </li>
+            <li>
+              <a href="#">Skills</a>
+            </li>
+            <li>
+              <a href="#">About Me</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
           </ul>
         </nav>
-        <div>
-          <h1>navbar</h1>
-        </div>
       </div>
     );
   }
